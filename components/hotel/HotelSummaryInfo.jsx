5552508ba6ba61737abd8c78b9aa16835cd35e3b
Nafisa Mila <jabinmila@gmail.com>
2025-06-22 15:39:59 +0600

    hotel list, summary updated

--- components/hotel/HotelSummaryInfo.jsx
+++ components/hotel/HotelSummaryInfo.jsx
@@ -1,23 +1,27 @@
-const HotelSummaryInfo = ({fromListPage}) => {
+import Link from "next/link";
+import HotelRating from "./HotelRating";
+import HotelReviewNumber from "./HotelReviewNumber";
+
+const HotelSummaryInfo = ({ fromListPage, info }) => {
   return (
     <>
       <div className={fromListPage ? "flex-1" : "flex-1 container"}>
-        <h2 className={fromListPage ? "font-bold text-lg" : "font-bold text-2xl"}>Effotel By Sayaji Jaipur</h2>
-        <p>📍 Kolkata</p>
+        <h2 className={fromListPage ? "font-bold text-lg" : "font-bold text-2xl"}>{info?.name}</h2>
+        <p>📍 {info?.city}</p>
         <div className="flex gap-2 items-center my-4">
-          <div className="w-[35px] h-[35px] rounded-sm text-white grid place-items-center font-bold" style={{ backgroundColor: "#FF6A28" }}>
-            5.3
-          </div>
-          <span className="font-medium">Very Good</span>
-          <span>232 Reviews</span>
+          <HotelRating id={info?.id} />
+          <HotelReviewNumber id={info?.id} />
+        </div>
+        <div>
+          <span className="bg-yellow-300 p-1 rounded-md">{info?.propertyCategory} Star Property</span>
         </div>
       </div>
 
       <div className="flex flex-col gap-2 items-end justify-center">
-        <h2 className="text-2xl font-bold text-right">$124/night</h2>
-        <p className=" text-right">Per Night for 4 Rooms</p>
+        <h2 className="text-2xl font-bold text-right">${(info?.highRate + info?.lowRate) / 2}/night</h2>
+        <p className=" text-right">Per Night for 1 Room</p>
         {
-          fromListPage ? (<button className="bg-primary px-8 py-2 rounded-md block text-white font-bold shadow-lg hover:shadow-primary/50 active:scale-95 transition-all" style={{ backgroundColor: "#FF6A28" }}>Details</button>) : (<button className="bg-primary px-4 py-2 rounded-md block text-white font-bold shadow-lg hover:shadow-primary/50 active:scale-95 transition-all" style={{ backgroundColor: "#FF6A28" }}>Book</button>)
+          fromListPage ? (<Link href={`/hotels/${info?.id}`} className="px-8 py-2 rounded-md block text-white font-bold shadow-lg hover:shadow-primary/50 active:scale-95 transition-all" style={{ backgroundColor: "#FF6A28" }}>Details</Link>) : (<button className="px-4 py-2 rounded-md block text-white font-bold shadow-lg hover:shadow-primary/50 active:scale-95 transition-all" style={{ backgroundColor: "#FF6A28" }}>Book</button>)
         }
       </div>
     </>
--- components/hotel/details/Gallery.jsx
+++ components/hotel/details/Gallery.jsx
@@ -1,14 +1,31 @@
-const Gallery = () => {
-  return (
-    <section className="container">
-      <div className="grid grid-cols-2 imageshowCase">
-        <img src="./assets/images/1.png" className="h-[400px]" alt="" />
+import Image from "next/image";
 
-        <div className="grid grid-cols-2 grid-rows-2 h-[400px]">
-          <img src="./assets/images/2.png" alt="" />
-          <img src="./assets/images/3.png" alt="" />
-          <img src="./assets/images/4.png" alt="" />
-          <img src="./assets/images/5.png" alt="" />
+const Gallery = ({gallery}) => {
+
+  const newGallery = [...gallery];
+  newGallery.shift();
+  return (
+    <section className="max-w-7xl mx-auto w-full px-4 py-8">
+      <div className="grid grid-cols-2 imageshowCase">
+        <Image
+          src={gallery[0]}
+          className="h-full w-full object-cover"
+          alt="Main Pic"
+          width={400}
+          height={400} />
+
+        <div className="grid grid-cols-2 grid-rows-2 h-full w-full object-cover">
+          {
+            newGallery.map((image) => (
+              <Image
+                key={image}
+                src={image}
+                className="h-[400px]"
+                alt="Sub Pics"
+                width={400}
+                height={400} />
+            ))
+          }
         </div>
       </div>
     </section>
