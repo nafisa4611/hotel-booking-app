9d65a697bc77022037f892c90c4152547c2c2df0
Nafisa Mila <jabinmila@gmail.com>
2025-07-04 16:12:51 +0600

    hotel search done

--- app/(home)/hotels/page.js
+++ app/(home)/hotels/page.js
@@ -2,18 +2,27 @@ import Search from "@/components/search/Search";
 import Filter from "@/components/search/Filter";
 import HotelList from "@/components/hotel/HotelList";
 
-const HotelListPage = () => {
+const HotelListPage = ({searchParams: {destination, checkin, checkout}}) => {
     return (
         <>
             <section className="bg-[url('/hero-bg.jpg')] bg-cover bg-no-repeat bg-center pt-[100px] pb-[60px]">
                 <div className="max-w-7xl mx-auto w-full px-4 items-center py-12 ">
-                    <Search fromList={true} />
+                    <Search 
+                        fromList={true}
+                        destination={destination}
+                        checkin={checkin}
+                        checkout={checkout}
+                    />
                 </div>
             </section>
             <section className="py-12">
                 <div className="max-w-7xl mx-auto w-full px-4 grid grid-cols-12">
                     <Filter />
-                    <HotelList />
+                    <HotelList 
+                        destination={destination}
+                        checkin={checkin}
+                        checkout={checkout}
+                    />
                 </div>
             </section>
         </>
--- components/search/Search.jsx
+++ components/search/Search.jsx
@@ -1,4 +1,55 @@
-const Search = ({fromList}) => {
+'use client';
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+
+const Search = ({ fromList, destination, checkin, checkout }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [searchTerm, setSearchTerm] = useState({
+    destination: destination || "Puglia",
+    checkin: checkin || "",
+    checkout: checkout || "",
+  });
+
+
+  const [allowSearch, setAllowSearch] = useState(true);
+
+  const handleInputs = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    const state = { ...searchTerm, [name]: value };
+
+    if (
+      new Date(state.checkin).getTime() >
+      new Date(state.checkout).getTime()
+    ) {
+      setAllowSearch(false);
+    } else {
+      setAllowSearch(true);
+    }
+    setSearchTerm(state);
+  };
+
+  function doSearch(event) {
+    const params = new URLSearchParams(searchParams);
+
+    params.set("destination", searchTerm?.destination || "all");
+    if (searchTerm?.checkin && searchTerm?.checkout) {
+      params.set("checkin", searchTerm?.checkin);
+      params.set("checkout", searchTerm?.checkout);
+    }
+
+    if (pathname.includes("hotels")) {
+      replace(`${pathname}?${params.toString()}`);
+    } else {
+      replace(`${pathname}hotels?${params.toString()}`);
+    }
+  }
+
+
   return (
     <>
       <div className="lg:max-h-[250px] mt-6">
@@ -6,12 +57,18 @@ const Search = ({fromList}) => {
           <div>
             <span>Destination</span>
             <h4 className="mt-2">
-              <select name="destination" id="destination" className="w-full px-4 py-2 rounded-md border border-black/20 bg-transparent">
-                <option value="Bali">Bali</option>
-                <option value="Bali">Cox's Bazar</option>
-                <option value="Bali">Sylhet</option>
-                <option value="Bali">Saint Martin</option>
-                <option value="Bali">Bali</option>
+              <select
+                name="destination"
+                id="destination"
+                defaultValue={searchTerm.destination}
+                className="w-full px-4 py-2 rounded-md border border-black/20 bg-transparent"
+                onChange={handleInputs}
+              >
+                <option value="Puglia">Puglia</option>
+                <option value="Catania">Catania</option>
+                <option value="Palermo">Palermo</option>
+                <option value="Frejus">Frejus</option>
+                <option value="Paris">Paris</option>
               </select>
             </h4>
           </div>
@@ -19,20 +76,41 @@ const Search = ({fromList}) => {
           <div>
             <span>Check in</span>
             <h4 className="mt-2">
-              <input type="date" name="checkin" id="checkin" className="w-full px-4 py-2 rounded-md border border-black/20 bg-transparent"/>
+              <input
+                type="date"
+                name="checkin"
+                id="checkin"
+                value={searchTerm.checkin}
+                className="w-full px-4 py-2 rounded-md border border-black/20 bg-transparent"
+                onChange={handleInputs}
+              />
             </h4>
           </div>
 
           <div>
             <span>Checkout</span>
             <h4 className="mt-2">
-              <input type="date" name="checkout" id="checkout" />
+              <input
+                type="date"
+                name="checkout"
+                id="checkout"
+                value={searchTerm.checkout}
+                onChange={handleInputs}
+                className="w-full px-4 py-2 rounded-md border border-black/20 bg-transparent"
+              />
             </h4>
           </div>
         </div>
       </div>
 
-      <button className="bg-primary px-8 py-3 rounded-md block mx-auto text-white font-bold -translate-y-1/2 shadow-lg hover:shadow-primary/50 active:scale-95 transition-all" style={{ backgroundColor: "#FF6A28" }}>🔍️ {fromList ? "Modify Search": "Search"}</button>
+      <button
+        disabled={!allowSearch}
+        onClick={doSearch}
+        className={`px-8 py-3 rounded-md block mx-auto text-white font-bold translate-y-1/2 shadow-lg hover:shadow-primary/50 active:scale-95 transition-all cursor-pointer ${allowSearch ? 'bg-[#FF6A28]' : 'bg-gray-400 cursor-not-allowed'
+          }`}
+      >
+        🔍️ {fromList ? "Modify Search" : "Search"}
+      </button>
     </>
   );
 };
